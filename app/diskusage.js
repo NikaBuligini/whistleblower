067be173edeba4193ca600bd3e688d494276568b
Nikoloz Buligini <nbuligini11@gmail.com>
2016-12-30 16:39:01 +0400

    test service requests

--- app/diskusage.js
+++ app/diskusage.js
@@ -1,4 +1,4 @@
-const disk = require('diskusage');
+// const disk = require('diskusage');
 const rp = require('request-promise');
 
 function formatBytes(bytes, decimals) {
@@ -12,28 +12,51 @@ function formatBytes(bytes, decimals) {
 
 module.exports = {
   check (path, callback) {
-    // get disk usage. Takes path as first parameter
-    disk.check(path, (err, info) => {
-      if (err) callback(err);
-
-      let options = {
-        method: 'POST',
-        url: 'http://localhost:8000/test',
-        form: info
+    const info = {
+      uuid: '30dedbd0-cb2c-11e6-a895-d9a489af7847',
+      data: {
+        available: 'test',
+        free: 'test',
+        total: 'test',
       }
+    }
 
-      rp(options)
-        .then((parsedBody) => {
-          // console.log(parsedBody);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    let options = {
+      method: 'POST',
+      url: 'http://localhost:8000/test',
+      form: info
+    }
 
-      Object.keys(info).map((key, index) => {
-        info[key] = formatBytes(info[key], 1);
+    rp(options)
+      .then((parsedBody) => {
+        // console.log(parsedBody);
       })
-      callback(null, info);
-    })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // get disk usage. Takes path as first parameter
+    // disk.check(path, (err, info) => {
+    //   if (err) callback(err);
+    //
+    //   let options = {
+    //     method: 'POST',
+    //     url: 'http://localhost:8000/test',
+    //     form: info
+    //   }
+    //
+    //   rp(options)
+    //     .then((parsedBody) => {
+    //       // console.log(parsedBody);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    //
+    //   Object.keys(info).map((key, index) => {
+    //     info[key] = formatBytes(info[key], 1);
+    //   })
+    //   callback(null, info);
+    // })
   }
 }
